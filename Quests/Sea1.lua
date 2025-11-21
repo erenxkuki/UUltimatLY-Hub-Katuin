8d58b588d32df5f92ff263631dc743f94acf1bf7
UUltimatLY <harshmeena072012@gmail.com>
2025-11-21 15:45:48 +0530

    Update Sea1.lua

--- Quests/Sea1.lua
+++ Quests/Sea1.lua
@@ -1,54 +1,32 @@
--- Quests/Sea1.lua | UUltimatLY Hub Kaitun | FULL First Sea (0 → 700+)
--- 100% Complete – Every quest, exact CFrames, tested Nov 2025
+-- Quests/Sea1.lua | UUltimatLY HUB | PERFECT First Sea Quests (Update 24 - Nov 2025)
 
-local Sea1 = {
-    -- Starter Island
-    {min = 0,   npc = CFrame.new(1061.67, 16.52, 1547.54), quest = "BanditQuest1",     qlv = 1, mob = "Bandit [Lv. 5]",        mobpos = CFrame.new(1199.31, 16.98, 1536.92)},
-    {min = 10,  npc = CFrame.new(-1405.05, 14.75, 44.78),  quest = "JungleQuest",      qlv = 1, mob = "Monkey [Lv. 14]",       mobpos = CFrame.new(-1598.09, 35.55, 153.38)},
-    {min = 15,  npc = CFrame.new(-1405.05, 14.75, 44.78),  quest = "JungleQuest",      qlv = 2, mob = "Gorilla [Lv. 20]",      mobpos = CFrame.new(-1425.0, 35.55, 103.0)},
-
-    -- Pirate Village
-    {min = 30,  npc = CFrame.new(1060.87, 16.62, 1548.3),   quest = "PirateQuest1",     qlv = 1, mob = "Pirate [Lv. 35]",       mobpos = CFrame.new(1142.69, 16.74, 1524.88)},
-    {min = 40,  npc = CFrame.new(1060.87, 16.62, 1548.3),   quest = "PirateQuest1",     qlv = 2, mob = "Brute [Lv. 45]",        mobpos = CFrame.new(1278.5, 16.74, 1480.2)},
-
-    -- Desert Island
-    {min = 60,  npc = CFrame.new(943.47, 6.49, 4223.51),    quest = "DesertQuest",      qlv = 1, mob = "Desert Bandit [Lv. 60]", mobpos = CFrame.new(925.0, 6.49, 4350.0)},
-    {min = 75,  npc = CFrame.new(943.47, 6.49, 4223.51),    quest = "DesertQuest",      qlv = 2, mob = "Desert Officer [Lv. 70]", mobpos = CFrame.new(895.0, 6.49, 4400.0)},
-
-    -- Frozen Village
-    {min = 90,  npc = CFrame.new(1129.87, 7.48, -1435.6),   quest = "SnowQuest",        qlv = 1, mob = "Snow Bandit [Lv. 90]",  mobpos = CFrame.new(1370.0, 87.0, -1300.0)},
-    {min = 100, npc = CFrame.new(1129.87, 7.48, -1435.6),   quest = "SnowQuest",        qlv = 2, mob = "Snowman [Lv. 100]",     mobpos = CFrame.new(1370.0, 104.0, -1420.0)},
-
-    -- Marine Fortress
-    {min = 120, npc = CFrame.new(-2850.0, 7.48, 5350.0),    quest = "MarineQuest2",     qlv = 1, mob = "Chief Petty Officer [Lv. 120]", mobpos = CFrame.new(-2900.0, 40.0, 5400.0)},
-
-    -- Skylands
-    {min = 150, npc = CFrame.new(-4970.0, 717.0, -2615.0),  quest = "SkyQuest",         qlv = 1, mob = "Sky Bandit [Lv. 150]",  mobpos = CFrame.new(-4950.0, 717.0, -2650.0)},
-    {min = 175, npc = CFrame.new(-4970.0, 717.0, -2615.0),  quest = "SkyQuest",         qlv = 2, mob = "Dark Master [Lv. 175]", mobpos = CFrame.new(-4900.0, 717.0, -2600.0)},
-
-    -- Prison
-    {min = 190, npc = CFrame.new(4858.0, 5.65, 735.0),      quest = "PrisonQuest",      qlv = 1, mob = "Prisoner [Lv. 190]",    mobpos = CFrame.new(4900.0, 5.65, 800.0)},
-    {min = 210, npc = CFrame.new(4858.0, 5.65, 735.0),      quest = "PrisonQuest",      qlv = 2, mob = "Dangerous Prisoner [Lv. 210]", mobpos = CFrame.new(5000.0, 5.65, 850.0)},
-
-    -- Colosseum
-    {min = 225, npc = CFrame.new(-1675.0, 7.48, -2925.0),   quest = "ColosseumQuest",   qlv = 1, mob = "Toga Warrior [Lv. 225]", mobpos = CFrame.new(-1600.0, 7.48, -3000.0)},
-    {min = 250, npc = CFrame.new(-1675.0, 7.48, -2925.0),   quest = "ColosseumQuest",   qlv = 2, mob = "Gladiator [Lv. 275]",   mobpos = CFrame.new(-1550.0, 7.48, -2950.0)},
-
-    -- Magma Village
-    {min = 300, npc = CFrame.new(-5235.0, 8.6, 8490.0),     quest = "MagmaQuest",       qlv = 1, mob = "Military Soldier [Lv. 300]", mobpos = CFrame.new(-5300.0, 8.6, 8550.0)},
-    {min = 325, npc = CFrame.new(-5235.0, 8.6, 8490.0),     quest = "MagmaQuest",       qlv = 2, mob = "Military Spy [Lv. 325]", mobpos = CFrame.new(-5800.0, 8.6, 8700.0)},
-
-    -- Underwater City
-    {min = 375, npc = CFrame.new(61200.0, 5.6, 1700.0),     quest = "FishmanQuest",     qlv = 1, mob = "Fishman Warrior [Lv. 375]", mobpos = CFrame.new(61100.0, 18.0, 1570.0)},
-    {min = 400, npc = CFrame.new(61200.0, 5.6, 1700.0),     quest = "FishmanQuest",     qlv = 2, mob = "Fishman Commando [Lv. 400]", mobpos = CFrame.new(61800.0, 18.0, 1400.0)},
-
-    -- Fountain City
-    {min = 625, npc = CFrame.new(5127.0, 6.0, 600.0),       quest = "FountainQuest",    qlv = 1, mob = "Galley Pirate [Lv. 625]", mobpos = CFrame.new(5400.0, 6.0, 800.0)},
-    {min = 650, npc = CFrame.new(5127.0, 6.0, 600.0),       quest = "FountainQuest",    qlv = 2, mob = "Galley Captain [Lv. 650]", mobpos = CFrame.new(5600.0, 6.0, 900.0)},
-
-    -- Final First Sea (to unlock Second Sea)
-    {min = 700, npc = CFrame.new(-461.0, 7.3, 3270.0),      quest = "MarineQuest3",     qlv = 1, mob = "Vice Admiral [Lv. 700]", mobpos = CFrame.new(-5000.0, 70.0, 4200.0)},
+_G.CurrentQuests = {
+    {min=0,   Mon="Bandit",                NameQuest="BanditQuest1",      LevelQuest=1,  CFrameQuest=CFrame.new(1059.37, 15.45, 1550.42),   CFrameMon=CFrame.new(1045.96, 27.00, 1560.82)},
+    {min=10,  Mon="Monkey",                NameQuest="JungleQuest",       LevelQuest=1,  CFrameQuest=CFrame.new(-1598.09, 35.55, 153.38),   CFrameMon=CFrame.new(-1448.52, 67.85, 11.47)},
+    {min=15,  Mon="Gorilla",               NameQuest="JungleQuest",       LevelQuest=2,  CFrameQuest=CFrame.new(-1598.09, 35.55, 153.38),   CFrameMon=CFrame.new(-1129.88, 40.46, -525.42)},
+    {min=30,  Mon="Pirate",                NameQuest="BuggyQuest1",       LevelQuest=1,  CFrameQuest=CFrame.new(-1141.07, 4.10, 3831.55),   CFrameMon=CFrame.new(-1103.51, 13.75, 3896.09)},
+    {min=40,  Mon="Brute",                 NameQuest="BuggyQuest1",       LevelQuest=2,  CFrameQuest=CFrame.new(-1141.07, 4.10, 3831.55),   CFrameMon=CFrame.new(-1140.08, 14.81, 4322.92)},
+    {min=60,  Mon="Desert Bandit",         NameQuest="DesertQuest",       LevelQuest=1,  CFrameQuest=CFrame.new(894.49, 5.14, 4392.43),      CFrameMon=CFrame.new(924.80, 6.45, 4481.59)},
+    {min=75,  Mon="Desert Officer",        NameQuest="DesertQuest",       LevelQuest=2,  CFrameQuest=CFrame.new(894.49, 5.14, 4392.43),      CFrameMon=CFrame.new(1608.28, 8.61, 4371.01)},
+    {min=90,  Mon="Snow Bandit",           NameQuest="SnowQuest",         LevelQuest=1,  CFrameQuest=CFrame.new(1389.74, 88.15, -1298.91),   CFrameMon=CFrame.new(1354.35, 87.27, -1393.95)},
+    {min=100, Mon="Snowman",               NameQuest="SnowQuest",         LevelQuest=2,  CFrameQuest=CFrame.new(1389.74, 88.15, -1298.91),   CFrameMon=CFrame.new(1201.64, 144.58, -1550.07)},
+    {min=120, Mon="Chief Petty Officer",   NameQuest="MarineQuest2",      LevelQuest=1,  CFrameQuest=CFrame.new(-5039.59, 27.35, 4324.68),  CFrameMon=CFrame.new(-4881.23, 22.65, 4273.75)},
+    {min=150, Mon="Sky Bandit",            NameQuest="SkyQuest",          LevelQuest=1,  CFrameQuest=CFrame.new(-4839.53, 716.37, -2619.44), CFrameMon=CFrame.new(-4953.21, 295.74, -2899.23)},
+    {min=175, Mon="Dark Master",           NameQuest="SkyQuest",          LevelQuest=2,  CFrameQuest=CFrame.new(-4839.53, 716.37, -2619.44), CFrameMon=CFrame.new(-5259.84, 391.40, -2229.04)},
+    {min=190, Mon="Prisoner",              NameQuest="PrisonerQuest",     LevelQuest=1,  CFrameQuest=CFrame.new(5308.93, 1.66, 475.12),     CFrameMon=CFrame.new(5098.97, -0.32, 474.24)},
+    {min=210, Mon="Dangerous Prisoner",    NameQuest="PrisonerQuest",     LevelQuest=2,  CFrameQuest=CFrame.new(5308.93, 1.66, 475.12),     CFrameMon=CFrame.new(5654.56, 15.63, 866.30)},
+    {min=250, Mon="Toga Warrior",          NameQuest="ColosseumQuest",    LevelQuest=1,  CFrameQuest=CFrame.new(-1580.05, 6.35, -2986.48),  CFrameMon=CFrame.new(-1820.21, 51.68, -2740.67)},
+    {min=275, Mon="Gladiator",             NameQuest="ColosseumQuest",    LevelQuest=2,  CFrameQuest=CFrame.new(-1580.05, 6.35, -2986.48),  CFrameMon=CFrame.new(-1292.84, 56.38, -3339.03)},
+    {min=300, Mon="Military Soldier",      NameQuest="MagmaQuest",        LevelQuest=1,  CFrameQuest=CFrame.new(-5313.37, 10.95, 8515.29),  CFrameMon=CFrame.new(-5411.16, 11.08, 8454.29)},
+    {min=325, Mon="Military Spy",          NameQuest="MagmaQuest",        LevelQuest=2,  CFrameQuest=CFrame.new(-5313.37, 10.95, 8515.29),  CFrameMon=CFrame.new(-5802.87, 86.26, 8828.86)},
+    {min=375, Mon="Fishman Warrior",       NameQuest="FishmanQuest",      LevelQuest=1,  CFrameQuest=CFrame.new(61122.65, 18.50, 1569.40),  CFrameMon=CFrame.new(60878.30, 18.48, 1543.76)},
+    {min=400, Mon="Fishman Commando",      NameQuest="FishmanQuest",      LevelQuest=2,  CFrameQuest=CFrame.new(61122.65, 18.50, 1569.40),  CFrameMon=CFrame.new(61922.63, 18.48, 1493.93)},
+    {min=450, Mon="God's Guard",           NameQuest="SkyExp1Quest",      LevelQuest=1,  CFrameQuest=CFrame.new(-4721.89, 843.87, -1949.97), CFrameMon=CFrame.new(-4710.04, 845.28, -1927.31)},
+    {min=475, Mon="Shanda",                NameQuest="SkyExp1Quest",      LevelQuest=2,  CFrameQuest=CFrame.new(-7859.10, 5544.19, -381.48), CFrameMon=CFrame.new(-7678.49, 5566.40, -497.22)},
+    {min=525, Mon="Royal Squad",           NameQuest="SkyExp2Quest",      LevelQuest=1,  CFrameQuest=CFrame.new(-7906.82, 5634.66, -1411.99), CFrameMon=CFrame.new(-7624.25, 5658.13, -1467.35)},
+    {min=550, Mon="Royal Soldier",         NameQuest="SkyExp2Quest",      LevelQuest=2,  CFrameQuest=CFrame.new(-7906.82, 5634.66, -1411.99), CFrameMon=CFrame.new(-7836.75, 5645.66, -1790.62)},
+    {min=625, Mon="Galley Pirate",         NameQuest="FountainQuest",     LevelQuest=1,  CFrameQuest=CFrame.new(5259.82, 37.35, 4050.03),   CFrameMon=CFrame.new(5551.02, 78.90, 3930.41)},
+    {min=650, Mon="Galley Captain",        NameQuest="FountainQuest",     LevelQuest=2,  CFrameQuest=CFrame.new(5259.82, 37.35, 4050.03),   CFrameMon=CFrame.new(5441.95, 42.50, 4950.09)},
 }
 
-_G.Sea1Quests = Sea1
-notify("FULL Sea 1 Table Loaded (0 → 700+) | 24 Quests Ready 🔥")
+notify("Sea1 Quests Loaded – 26 Perfect Quests | UUltimatLY HUB")
